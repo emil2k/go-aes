ba6a3e022bfeca061b8138ea583e82e806b003d6
Emil <emil2k@gmail.com>
2014-07-17 13:26:09 +0200

    Decryption from command line, input from file.
    Can encrypt or decrypt from command line by calling `go-aes key.file
    input.file output.file`, for decryption add -d flag. Removed randomly
    generated input data now input comes from an input file.
    
    Decryption extracts the nonce and encrypted data from the custom file
    format.

--- aes.go
+++ aes.go
@@ -5,13 +5,10 @@ import (
 	"encoding/hex"
 	"flag"
 	"log"
-	mrand "math/rand"
 	"os"
-	"time"
 
 	"github.com/emil2k/go-aes/cipher"
 	"github.com/emil2k/go-aes/modes/ctr"
-	"github.com/emil2k/go-aes/state"
 )
 
 // Logs
@@ -22,32 +19,38 @@ var debugLog *log.Logger = log.New(os.Stdout, "debug : ", 0)
 // Command line arguments
 var verbose bool
 var veryVerbose bool
+var isDecrypt bool
 var mode string
+var args []string
+var key string
+var input string
 var output string
-var keyOutput string
 
 func main() {
 	// Parse command flags
 	flag.BoolVar(&verbose, "v", false, "verbose output, debugging from block cipher mode")
 	flag.BoolVar(&veryVerbose, "vv", false, "very verbose output, includes debugging from block cipher")
+	flag.BoolVar(&isDecrypt, "d", false, "whether in encryption mode")
 	flag.StringVar(&mode, "mode", "ctr", "block cipher mode, `ctr` for counter mode")
-	flag.StringVar(&output, "o", "output.aes", "output file for the encrypted file")
-	flag.StringVar(&keyOutput, "k", "output.key.aes", "output file for the encryption key")
 	flag.Parse()
 	if veryVerbose {
 		verbose = true
 	}
+	args = flag.Args()
+	if len(args) < 3 {
+		errorLog.Fatalln("must specify the key, input, output paths for both encryption and decrytption")
+	}
+	key, input, output = args[0], args[1], args[2]
 	if verbose {
 		debugLog.Println("verbose : ", verbose)
 		debugLog.Println("very verbose : ", veryVerbose)
 		debugLog.Println("mode : ", mode)
-		debugLog.Println("args : ", flag.Args())
+		debugLog.Println("is decryption? : ", isDecrypt)
+		debugLog.Println("key : ", key)
+		debugLog.Println("output : ", output)
+		debugLog.Println("input : ", input)
+		debugLog.Println("args : ", args)
 	}
-	// Open up output files
-	ofile := createFile(output)
-	defer closeFile(ofile)
-	ofileKey := createFile(keyOutput)
-	defer closeFile(ofileKey)
 	// Setup cipher
 	c := cipher.NewCipher(4, 10)
 	c.ErrorLog = errorLog
@@ -57,13 +60,31 @@ func main() {
 			c.DebugLog = debugLog
 		}
 	}
-	// Generate random input data
-	mrand.Seed(time.Now().UnixNano())
-	in := getRand(mrand.Intn(1000)) // random length input
-	c.DebugLog.Println(len(in), "bytes in input")
-	// Generate random cipher key
-	ck := getRand(16)
-	c.DebugLog.Println(state.State(ck), "cipher key")
+	// Setup file handlers
+	var kfile, ifile, ofile *os.File
+	if isDecrypt {
+		kfile, ifile, ofile = openFile(key), openFile(input), createFile(output)
+	} else {
+		kfile, ifile, ofile = createFile(key), openFile(input), createFile(output)
+	}
+	defer closeFile(kfile)
+	defer closeFile(ifile)
+	defer closeFile(ofile)
+	// Initiate data
+	var ck, in, nonce []byte
+	if isDecrypt {
+		ck = readFromFile(kfile)
+		nonce, in = inputFromFile(ifile)
+	} else {
+		// Generate random cipher key
+		ck = getRand(16)
+		in = readFromFile(ifile)
+		nonce, _ = ctr.NewNonce()
+	}
+	if verbose {
+		c.DebugLog.Println(hex.EncodeToString(ck), "cipher key")
+		c.DebugLog.Println(hex.EncodeToString(nonce), "nonce")
+	}
 	// Setup and run the appropriate block cipher mode
 	switch mode {
 	case "ctr", "cm", "icm", "sic":
@@ -74,19 +95,19 @@ func main() {
 		if verbose {
 			ctrMode.DebugLog = debugLog
 		}
-		nonce, _ := ctr.NewNonce()
-		ct := ctrMode.Encrypt(in, ck, nonce)
-		if verbose {
-			debugLog.Printf("cipher text after counter mode encryption\n%s\n", hex.Dump(ct))
+		if isDecrypt {
+			out := ctrMode.Decrypt(in, ck, nonce)
+			writeToFile(ofile, out...)
+		} else {
+			outputToFile(ofile, nonce, ctrMode.Encrypt(in, ck, nonce))
+			infoLog.Println("encryption stored in", ofile.Name())
+			// Output cipher key
+			writeToFile(kfile, ck...)
+			infoLog.Println("cipher key stored in", kfile.Name())
 		}
-		outputToFile(ofile, nonce, ct)
-		infoLog.Println("encryption stored in", ofile.Name())
 	default:
 		errorLog.Fatalln("unknown mode chosen")
 	}
-	// Output cipher key
-	writeToFile(ofileKey, ck...)
-	infoLog.Println("cipher key stored in", ofileKey.Name())
 }
 
 // outputToFile outputs the encrypted data into a custom AES file format
@@ -99,12 +120,41 @@ func outputToFile(f *os.File, iv []byte, data []byte) {
 	writeToFile(f, data...)
 }
 
+// inputFromFile parses the custom AES file format returning the nonce and
+// encrypted message
+func inputFromFile(f *os.File) ([]byte, []byte) {
+	data := readFromFile(f)
+	ivl := int(data[0])
+	return data[1 : 1+ivl], data[1+ivl:]
+}
+
 // writeToFile writes the data bytes to given file outputting any errors
 // to the error log
 func writeToFile(f *os.File, data ...byte) {
 	if _, err := f.Write(data); err != nil {
 		errorLog.Panicln(err)
 	}
+}
+
+// readFromFile reads data from given file into a byte slice
+// outputs any errors to the error log maximum size 1024 bytes
+func readFromFile(f *os.File) []byte {
+	var fsize int64
+	if finfo, err := f.Stat(); err != nil {
+		errorLog.Panicln(err)
+		return nil
+	} else if fsize = finfo.Size(); fsize > 1024 {
+		errorLog.Panicln("file is to large to read, file size :", fsize)
+		return nil
+	}
+	data := make([]byte, fsize)
+	if n, err := f.Read(data); err != nil {
+		errorLog.Panicln(err)
+		return nil
+	} else if verbose {
+		debugLog.Println(n, "bytes read from", f.Name())
+	}
+	return data
 }
 
 // createFile creates a file, truncates an existing file outputs errors
@@ -116,6 +166,19 @@ func createFile(name string) *os.File {
 	} else {
 		if verbose {
 			debugLog.Println("created file", f.Name())
+		}
+		return f
+	}
+}
+
+// openFile opens a file outputs errors to the the error log
+func openFile(name string) *os.File {
+	if f, err := os.Open(name); err != nil {
+		errorLog.Panicln(err)
+		return nil
+	} else {
+		if verbose {
+			debugLog.Println("opened file", f.Name())
 		}
 		return f
 	}
